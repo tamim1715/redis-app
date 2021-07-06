de5e4a2c6dd7f4687bf0802b3172a13a455cbb7d
arafat <arafat.khan@klovercloud.com>
2021-07-06 20:53:29 +0600

    redisCustomError bug fixed -3

--- api/v1/cache.go
+++ api/v1/cache.go
@@ -67,19 +67,20 @@ func (e CacheControllerInstance) Delete(c echo.Context) error {
 	params := c.QueryParams()
 	if params.Get("key") == "" {
 		log.Println("key not found in params")
-		return c.JSON(http.StatusBadRequest, errors.New("key missing in query param"))
+		return c.JSON(http.StatusBadRequest, "key missing in query param")
 	}
 	_, rdsErr := rds.GetRedisMaster().Get(context.Background(), params.Get("key")).Result()
 	if rdsErr == redis.Nil {
 		log.Println("key not found")
-		return c.JSON(http.StatusBadRequest, errors.New("key not found"))
+		return c.JSON(http.StatusBadRequest, "key not found")
 	} else {
-		err := rds.GetRedisMaster().Del(context.Background(), params.Get("key"))
+		err := rds.GetRedisMaster().Del(context.Background(), params.Get("key")).Err()
 		if err != nil {
-			log.Println("key deletion failed")
-			return c.JSON(http.StatusBadRequest, errors.New("key update failed: "+err.String()))
+			log.Println("key deletion failed: " + err.Error())
+			return c.JSON(http.StatusBadRequest, "key update failed: "+err.Error())
 		}
 	}
+
 	return c.JSON(http.StatusOK, nil)
 }
 
@@ -92,7 +93,7 @@ func (e CacheControllerInstance) Get(c echo.Context) error {
 	value, rdsErr := rds.GetRedisMaster().Get(context.Background(), params.Get("key")).Result()
 	if rdsErr == redis.Nil {
 		log.Println("key not found")
-		return c.JSON(http.StatusBadRequest, errors.New("key not found"))
+		return c.JSON(http.StatusBadRequest, "key not found")
 	}
 
 	respObj := dto.RedisObject{Key: params.Get("key"), Value: value}
